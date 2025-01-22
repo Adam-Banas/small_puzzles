476c24f2cfda9d506a44d0a4bd035dffb3fce28a
Adam Banas <adam.banas.1989@gmail.com>
2025-01-22 13:21:40 +0100

    LCS puzzle basic solution
    This solution doesn't use memoization, so the LongRunningTest takes
    a long time to finish.

--- longest_common_subsequence/solution/solution.cpp
+++ longest_common_subsequence/solution/solution.cpp
@@ -1,5 +1,15 @@
 #include "solution.hpp"
 
+// Basic LCS solution using recursion.
+// This solution doesn't use memoization, so a lot of computations are repeated.
 int lcs(std::string_view a, std::string_view b) {
-    return 0;
+    if (a.empty() || b.empty()) {
+        return 0;
+    }
+
+    if (a[0] == b[0]) {
+        return 1 + lcs(a.substr(1), b.substr(1));
+    }
+
+    return std::max(lcs(a.substr(1), b), lcs(a, b.substr(1)));
 }
